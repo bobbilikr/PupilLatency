d5bbf97e7e201b7a3ae3470ec8b2f20230ab4033
System Administrator <root@Sujeath-2.local>
2014-09-22 17:53:15 +0530

    testing snapshots

--- pre.m
+++ pre.m
@@ -1,7 +1,7 @@
-imaqhwinfo                              % Stands for image acquisition hardware info
-cam=imaqhwinfo;
-cam.InstalledAdaptors                   % Shows the data about the Installed adaptors
-dev_info = imaqhwinfo('winvideo',1)     % info about the Device
+
 vid=videoinput('winvideo', 1,'YUY2_320x240');          % Video Parameters
 set(vid,'ReturnedColorSpace','grayscale');      % acquire in greyscale
-preview(vid);                           % Preview
+while(1)
+    imshow(getsnapshot(vid));
+    pause(0.01);
+end% Preview
