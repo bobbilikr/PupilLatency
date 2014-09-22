1523746c0f56e3ff416f047d7aea875c38d412bf
karthikreddy745 <karthikreddy745@gmail.com>
2014-09-22 17:39:55 +0530

    greyscale

--- pre.m
+++ pre.m
@@ -3,4 +3,5 @@ cam=imaqhwinfo;
 cam.InstalledAdaptors                   % Shows the data about the Installed adaptors
 dev_info = imaqhwinfo('winvideo',1)     % info about the Device
 vid=videoinput('winvideo', 1,'YUY2_320x240');          % Video Parameters
+set(vid,'ReturnedColorSpace','grayscale');      % acquire in greyscale
 preview(vid);                           % Preview
