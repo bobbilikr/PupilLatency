3ec0a6d8c4bc731df97c30343821db419a57803c
Sujeath Pareddy <sujeath@Sujeath-2.local>
2014-09-22 18:28:04 +0530

    testing snapshots

--- pre.m
+++ pre.m
@@ -3,9 +3,16 @@ vid=videoinput('winvideo', 1,'YUY2_320x240');          % Video Parameters
 set(vid,'ReturnedColorSpace','grayscale');      % acquire in greyscale
 triggerconfig(vid, 'manual');
 start(vid);
-figure;
+gcf=figure;
+set(gcf,'CloseRequestFcn',@my_closefcn)
 hold on;
-while(1)
+closeflag=1;
+while(closeflag)
     imshow(getsnapshot(vid));
     pause(0.01);
 end% Preview
+
+function my_closefcn()
+    closeflag=0;
+    delete(gcf);
+end
