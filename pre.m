bc5bc7666517226b684f7c0c2755cda3c0d88fd2
System Administrator <root@Sujeath-2.local>
2014-09-22 18:01:48 +0530

    testing snapshots

--- pre.m
+++ pre.m
@@ -1,6 +1,8 @@
 
 vid=videoinput('winvideo', 1,'YUY2_320x240');          % Video Parameters
 set(vid,'ReturnedColorSpace','grayscale');      % acquire in greyscale
+figure;
+hold on;
 while(1)
     imshow(getsnapshot(vid));
     pause(0.01);
